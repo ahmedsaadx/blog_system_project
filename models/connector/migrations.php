47fc8d868453528719a4126799b93f29db9ee8f2
ahmedsaadx <ahmeddsaadd188@gmail.com>
2024-10-22 14:29:46 +0300

    split handler from migrations

---
+++ models/connector/migrations.php
@@ -0,0 +1,6 @@
+<?php
+require_once("handler.php");
+require_once(__DIR__ .'/../queries/categories.table.php');
+require_once(__DIR__ . '/../queries/messages.table.php');
+require_once(__DIR__ .'/../queries/users.table.php');
+require_once(__DIR__ .'/../queries/post.table.php');
