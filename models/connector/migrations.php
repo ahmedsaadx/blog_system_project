97982fa2d318ad981076039aa2df033114869e11
ahmedsaadx <ahmeddsaadd188@gmail.com>
2024-10-22 23:50:04 +0300

    dev

--- models/connector/migrations.php
+++ models/connector/migrations.php
@@ -1,5 +1,7 @@
 <?php
-require_once("handler.php");
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once ("../../index.php");
 require_once(__DIR__ .'/../queries/categories.table.php');
 require_once(__DIR__ . '/../queries/messages.table.php');
 require_once(__DIR__ .'/../queries/users.table.php');
